fa32e993691cbc788020b7921762f5c10c3d3844
Sergio Otero Castro <a22sergiooc@iessanclemente.net>
2024-03-08 14:32:40 +0100

    08/03 14:35

---
+++ LEMP/dev/app/app/config/parameters.php
@@ -0,0 +1,64 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'database',
+    'database_port' => '',
+    'database_name' => 'db',
+    'database_user' => 'user',
+    'database_password' => 'user',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => '3fYbwJXOnSQSKDsku5K9PV2P60vaxSkoVfmveCR1v154tTeF8HVsD3Mgo3uadjkR',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2024-03-08',
+    'locale' => 'es-ES',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'N90o0MkW7HoZ7cmOcouKXb0C5YfHsswNBP2KO6YNYxz1C5Go85fJt884udSAcQ1z',
+    'cookie_iv' => 'm1qT3kBAULJnTBN0r8D67mPWBa77Fnfn',
+    'new_cookie_key' => 'def000008323268787edc5c97a6e5c350e022fda64576748a59431b4234f084cf611adc3ae5f5167ef29641217fb625868c4289796e086cee898a0b41dc1cc0c3118c18f',
+    'api_public_key' => '-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvV1O5pDxCU+IZbXFvvXt
+rgsXRgmgxa+F3dXU0GDlAk8BKEyNjjW5nE+PQS9feuHJ8FK2J72Bzw+f6acZnpug
+ptv5Q8z2pSw0Q//fU74gBlLLunKjE0nseyOdkb5b/6K99uhw6Bli6grH8sIu3cvq
+V95ty4t6B/6gZlADboyeWfsfBpJYvxusjuAQNr9lGaiQgQPpSkjs6JH2+eqBNn+Q
+CyD4AcZqxn9NcfpCQ/V+MqS/lExGjVOdEuQv0BgrTKh8wj/Vlc1Y+nspiJUAAg6N
+Xh0ZlkIdDjwS26ZxfhUvOsjGxr95eVqoaRSykwBP08+uqP7WRLtlutaaRinDx56M
+MwIDAQAB
+-----END PUBLIC KEY-----
+',
+    'api_private_key' => '-----BEGIN PRIVATE KEY-----
+MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQC9XU7mkPEJT4hl
+tcW+9e2uCxdGCaDFr4Xd1dTQYOUCTwEoTI2ONbmcT49BL1964cnwUrYnvYHPD5/p
+pxmem6Cm2/lDzPalLDRD/99TviAGUsu6cqMTSex7I52Rvlv/or326HDoGWLqCsfy
+wi7dy+pX3m3Li3oH/qBmUANujJ5Z+x8Gkli/G6yO4BA2v2UZqJCBA+lKSOzokfb5
+6oE2f5ALIPgBxmrGf01x+kJD9X4ypL+UTEaNU50S5C/QGCtMqHzCP9WVzVj6eymI
+lQACDo1eHRmWQh0OPBLbpnF+FS86yMbGv3l5WqhpFLKTAE/Tz66o/tZEu2W61ppG
+KcPHnowzAgMBAAECggEAD5jd5NPPZqrsWyRLs1ziRNGZCyXD3kSEzC7OppM0iaok
+OEOF7U6ziRJshDv4q1Bpp6fIZSGQzZNF4qLm00+SPewkbdc2AYjQjUFIDce9L4gm
+S6chk3clSUc3tUduoHGxH+MpWZSttwVITtB7t70SAkbJ2FgWpw3r/EFe5v4iFNsb
+/u7Mv5vNXpNn1gkrzoboI8RiYPj170QyqX03Wudnb1jQNeUtG3Xs8sNp1FuDdyFj
+mO1Oubq03jgAgayA2EDn4b3W4xUq3ORsBqytzFCw+8pZXIRLLhcmXgCP0EAo6HUi
+Yf9a0jiDczty/iEuh12oMyJpybc4amCTx+aDh/eYkQKBgQDfVnUhHkbL0X/o24O3
+poL4+zps/iRbQ9AZ9c3S2ML1EOdtsIXAoM+grk27eq6BcBDDisWTktJ134EbAYZ4
+bRTRADRmVQewoq89fdCqu6F4PRl+ov1PRn7k+4uT9CJpF2bHxY8LotOQ5CEa8uA6
+S+dLPy2V8brvSymyJfIMtuD8RQKBgQDZDu0ku9ZxpwySR1yZEkVuCo1YABrBoHuV
+OsBaIjrVp2izo2v2qTGx2oP/WfRhpl36+peBCc1iV9zt1sXvuyqSHig9OS54KAav
+ec2C2cy5eyc2168M1lSJuvl8qJYM9jtyemt7U6qU1AOEM4OM68akcWBWCetmVLDi
+CGV1sON6FwKBgQC9DT6RDkP3fBu+TTMuXnE3udI3ONoltz2M/BmChaJiODDvFSHj
+cTzQUzvRm4PkZayLGgktZndo+A0dgykE/7FDSd0RUqDz0IJulvXv0Gxaj73pCKdM
+V+xKgPQiBeKq+vgx2vW/b9GPMM0lNrrAHkhn1c8MwlbvAL0G5EAEqzm/0QKBgQCT
+e7LiTMcuviSql9ZxI5sNZBoZ+ygy/Eo8YYklNTDozzEzLg1Dsj7T6/fYX1TPrO0a
+hX6/7NsiwTSaXl5rv67zTY5l2x6aAghY3tgm2rpiJS3F89M7U75pPyQZ8arS79QA
+uvGhAH5dVZl2EFKIRAPa5DZi/6Z1B4KQcsgNqb1GzQKBgQDYebXCId9m0MeJDQvE
+Ipwk+zEjAYXW5aFwbFuqXSfaY2i9iR187QthwkENt5Jw+hmmNDVphOQ8zz+CWIk5
+IcU1cwpJqMRFkFvkKUZEq1wpG62Z81dE7IzZbKujVAH58SDP9IyKWFYdUR5lGSNw
+EnesZYEKRUFVSIG1cMo4Bpjbog==
+-----END PRIVATE KEY-----
+',
+  ),
+);
