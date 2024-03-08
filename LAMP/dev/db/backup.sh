aa388c46642187424db3ca1094c4b3400d18c00a
Sergio Otero Castro <a22sergiooc@iessanclemente.net>
2024-03-08 09:29:11 +0100

    Dev terminado, prod en proceso

--- LAMP/dev/db/backup.sh
+++ LAMP/dev/db/backup.sh
@@ -1,1 +1,1 @@
-docker compose exec basedatos /bin/bash -c "mariadb-dump -u user -puser db" > ./db/backup.sql
+docker compose exec database /bin/bash -c "mariadb-dump -u user -puser db" > ./db/backup.sql
